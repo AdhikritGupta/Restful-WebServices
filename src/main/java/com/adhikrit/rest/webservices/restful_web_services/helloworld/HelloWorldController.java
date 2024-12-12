54f4bb750759dc0cd377f58b8110c412c4be260b
AdhikritGupta <adhikrit2003@gmail.com>
2024-12-12 10:36:08 +0530

    Next commit

--- src/main/java/com/adhikrit/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/adhikrit/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.adhikrit.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
 @RestController
 public class HelloWorldController {
 
-	
+	 
 //	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -17,6 +18,17 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	
+	//Path Parameters
+	// /users/{id}/todos/{id} => /users/2/todos/200
+	// /hello-world/path-variable/{name}
+	// /hello-world/path-variable/Adhikrit
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+//		return new HelloWorldBean("Hello World "+name);
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 	
 }
---
+++ src/main/java/com/adhikrit/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,44 @@
+package com.adhikrit.rest.webservices.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+//	JPA/Hibernate > Database
+//	UserDaoService > Static List
+	private static List<User> users = new ArrayList<>();
+	private static int num;
+	static {
+		users.add(new User(++num, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(++num, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(++num, "Jim", LocalDate.now().minusYears(20)));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	public User findById(int id) {
+//		for (User user : users) {
+//			if (user.getId()==id) {
+//				return user;
+//			}
+//		}
+//		return null;
+		Predicate<? super User> predicate = user -> user.getId().equals(id); 
+		return users.stream().filter(predicate).findFirst().orElse(null);
+		
+	}
+	
+	public User save(User user) {
+		user.setId(++num);
+		users.add(user);
+		return user;
+	}
+	
+}
